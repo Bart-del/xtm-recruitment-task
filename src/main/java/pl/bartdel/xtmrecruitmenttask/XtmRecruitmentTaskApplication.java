688764de0ac5ad77b45e39171cfc477cd1e31269
Bartłomiej Wysocki <bartlomiej.wyso@gmail.com>
2021-12-07 15:36:43 +0100

    initial commit

---
+++ src/main/java/pl/bartdel/xtmrecruitmenttask/XtmRecruitmentTaskApplication.java
@@ -0,0 +1,13 @@
+package pl.bartdel.xtmrecruitmenttask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XtmRecruitmentTaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XtmRecruitmentTaskApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/bartdel/xtmrecruitmenttask/XtmRecruitmentTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.bartdel.xtmrecruitmenttask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XtmRecruitmentTaskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
