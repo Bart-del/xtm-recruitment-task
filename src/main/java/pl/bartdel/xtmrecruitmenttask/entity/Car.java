0641d3310d8f28f4835e31ec008fdbaaf494344b
Bartłomiej Wysocki <bartlomiej.wyso@gmail.com>
2021-12-07 15:47:44 +0100

    create car entity

--- src/main/java/pl/bartdel/xtmrecruitmenttask/entity/Car.java
+++ src/main/java/pl/bartdel/xtmrecruitmenttask/entity/Car.java
@@ -1,4 +1,22 @@
 package pl.bartdel.xtmrecruitmenttask.entity;
 
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
 public class Car {
+    @Id
+    private Long id;
+    private String model;
+    private Producer producer;
+    private boolean isRented;
 }
---
+++ src/main/java/pl/bartdel/xtmrecruitmenttask/entity/Producer.java
@@ -0,0 +1,4 @@
+package pl.bartdel.xtmrecruitmenttask.entity;
+
+public enum Producer {
+}
