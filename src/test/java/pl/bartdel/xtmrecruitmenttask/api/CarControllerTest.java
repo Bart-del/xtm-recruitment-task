1c0416a7b06a44348a021175a7e711b82e2cc454
Bartłomiej Wysocki <bartlomiej.wyso@gmail.com>
2021-12-09 14:55:17 +0100

    add 'addNewCar' method tests

--- src/test/java/pl/bartdel/xtmrecruitmenttask/api/CarControllerTest.java
+++ src/test/java/pl/bartdel/xtmrecruitmenttask/api/CarControllerTest.java
@@ -3,21 +3,20 @@ package pl.bartdel.xtmrecruitmenttask.api;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import pl.bartdel.xtmrecruitmenttask.entity.Car;
 import pl.bartdel.xtmrecruitmenttask.entity.Producer;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 import java.time.Year;
 
 @SpringBootTest
 public class CarControllerTest {
 
-
-    CarController carController;
-
     @Autowired
-    CarControllerTest(CarController carController){
-        this.carController = carController;
-    }
+    private CarController carController;
 
     @Test
     public void addNewCar_Success(){
@@ -26,8 +25,21 @@ public class CarControllerTest {
         car.setModel("A17");
         car.setYearOfProduction(Year.of(2015));
         car.setProducer(Producer.BMW);
-
-        carController.addNewCar(car);
-
+        ResponseEntity<?> response = carController.addNewCar(car);
+        assertThat(response.getStatusCode()).isEqualByComparingTo(HttpStatus.OK);
     }
+
+    @Test
+    public void addNewCar_Failure(){
+        Car car = new Car();
+        car.setId(null);
+        car.setModel(null);
+        car.setYearOfProduction(null);
+        car.setProducer(null);
+
+        ResponseEntity<?> response = carController.addNewCar(car);
+        assertThat(response.getStatusCode()).isEqualByComparingTo(HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+
 }
