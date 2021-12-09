23cea8efaff8e53a35b1f1e604b7a547e8308546
Bartłomiej Wysocki <bartlomiej.wyso@gmail.com>
2021-12-09 19:00:23 +0100

    remove import from CarController

--- src/main/java/pl/bartdel/xtmrecruitmenttask/api/CarController.java
+++ src/main/java/pl/bartdel/xtmrecruitmenttask/api/CarController.java
@@ -1,6 +1,5 @@
 package pl.bartdel.xtmrecruitmenttask.api;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import pl.bartdel.xtmrecruitmenttask.entity.Car;
